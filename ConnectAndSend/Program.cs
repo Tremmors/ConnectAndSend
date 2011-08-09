9c14c26e55c066c1006b184aacf168ee10e2526f
johntrimis <johntrimis@gmail.com>
2011-08-08 22:34:54 -0400

    Final v 1.0

--- ConnectAndSend/Program.cs
+++ ConnectAndSend/Program.cs
@@ -1,57 +1,125 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) 2011 John Trimis 
+//
+// MIT license:
+// Permission is hereby granted, free of charge, to any person obtaining a copy of 
+// this software and associated documentation files (the "Software"), to deal in 
+// the Software without restriction, including without limitation the rights to 
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+// the Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all 
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
 namespace ConnectAndSend
-{
+{   // namespace ConnectAndSend
+
+    /// <summary>
+    ///     ConnectAndSend.exe
+    ///     Connects out to a TCP/IP connection and sends a specific string before disconnecting.
+    /// </summary>
     class Program
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length < 3)
-            {
+    {   // class Program
+
+        /// <summary>
+        ///     Main execution path
+        /// </summary>
+        /// <param name="args">
+        ///     Command Line arguments
+        ///         args[0] = Hostname or IP to connect to
+        ///         args[1] = Port to connect on
+        ///         args[2] = data to send once connected.
+        /// </param>
+        static void Main( string[] args )
+        {   // Main
+
+            if ( args.Length < 3 )
+            {   // Not enough arguments
+
                 HowDoIUseThis();
-            }
+
+            }   // Not enough arguments
             else
-            {
-                string strHost = args[0];
-                string strPort = args[1];
-                string strCommand = args[2];
-                strCommand = strCommand.Replace(@"\n", "\n").Replace(@"\\", "\\").Replace(@"\r", "\r").Replace("\\\"","\"");
+            {   // Proper number of arguments
+
+                string strHost      = args[0];
+                string strPort      = args[1];
+                string strCommand   = args[2];
                 int iPort;
-                if (int.TryParse(strPort, out iPort))
-                {
-                    if (iPort < 1 || iPort > 65535)
-                    {
+
+                strCommand = strCommand.Replace( @"\n", "\n" ).Replace( @"\\", "\\" ).Replace( @"\r", "\r" ).Replace( "\\\"", "\"" );
+                
+                if ( int.TryParse( strPort, out iPort ) )
+                {   // Numeric port number
+
+                    if ( iPort < 1 || iPort > 65535 )
+                    {   // Invalid Port Number
+
                         HowDoIUseThis();
-                    }
+
+                    }   // Invalid Port Number
                     else
-                    {
-                        Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                        Encoding enc = Encoding.GetEncoding("Windows-1252");
-                        sock.Connect(strHost, iPort);
-                        sock.Send(enc.GetBytes(strCommand));
-                        sock.Close();
-                    }
-                }
+                    {   // Valid Port Number
+
+
+                        try
+                        {   // TRY
+
+                            Socket sock = new Socket( AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
+                            Encoding enc = Encoding.GetEncoding( "Windows-1252" );
+                            sock.Connect( strHost, iPort );
+                            sock.Send( enc.GetBytes( strCommand ) );
+                            sock.Close();
+
+                        }   // TRY
+                        catch (Exception ex)
+                        {   // CATCH : Exception
+
+                            Console.WriteLine( string.Format( "Error when connecting.  Error:{0}", ex.Message ) );
+
+                        }   // CATCH : Exception
+
+                    }   // Valid Port Number
+
+                }   // Numeric port number
                 else
-                {
+                {   // Non-Numeric port number
+
                     HowDoIUseThis();
-                }
-            }
-        }
+
+                }   // Non-Numeric port number
+            }   // Proper number of arguments
+
+        }   // Main
+
         static void HowDoIUseThis()
-        {
-            Console.WriteLine("ConnectAndSend.exe [host] [port] \"[command]\"");
-            Console.WriteLine("\thost\t: The Hostname or IP to connect to");
-            Console.WriteLine("\tport\t: The Port to connect to (1-65535)");
-            Console.WriteLine("\tcommand\t: The command to execute (should probably be in quotes)");
-            Console.WriteLine("\t\tNote: allows the following escape sequences: ");
-            Console.WriteLine("\t\t\t\\r - Carriage Return");
-            Console.WriteLine("\t\t\t\\n - Line Feed");
-            Console.WriteLine("\t\t\t\\\" - Quote character");
-        }
-    }
-}
+        {   // HowDoIUseThis
+
+            Console.WriteLine( "ConnectAndSend.exe [host] [port] \"[command]\"" );
+            Console.WriteLine( "\t Connects to a specified TCP/IP port and sends some specific " );
+            Console.WriteLine( "\t data before disconnecting" );
+            Console.WriteLine( "\thost\t: The Hostname or IP to connect to" );
+            Console.WriteLine( "\tport\t: The Port to connect to (1-65535)" );
+            Console.WriteLine( "\tcommand\t: The command to execute (should probably be in quotes)" );
+            Console.WriteLine( "\t\tNote: allows the following escape sequences: " );
+            Console.WriteLine( "\t\t\t\\r - Carriage Return" );
+            Console.WriteLine( "\t\t\t\\n - Line Feed" );
+            Console.WriteLine( "\t\t\t\\\" - Quote character" );
+
+        }   // HowDoIUseThis
+
+    }   // class Program
+
+}   // namespace ConnectAndSend
